835ea4582f7f162de951a6a5cef57846954e22af
Ruoxin Lin <ruoxinlin.ltg@gmail.com>
2019-07-24 03:45:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ function add2(n) {
   // Feel free to move things around!
   return n + two
 }
-var theFunk = funkyFunction()();
 
 function funkyFunction() {
   return function() {
     return "FUNKY!"
   }
 }
+var theFunk = funkyFunction()();
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
