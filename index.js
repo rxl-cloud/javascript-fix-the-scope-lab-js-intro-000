40ab27ab108afd66bc894b3027c50104e29f9a5a
Ruoxin Lin <ruoxinlin.ltg@gmail.com>
2019-07-24 03:47:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function funkyFunction() {
     return "FUNKY!"
   }
 }
-var theFunk = funkyFunction()
+ theFunk = funkyFunction()()
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
